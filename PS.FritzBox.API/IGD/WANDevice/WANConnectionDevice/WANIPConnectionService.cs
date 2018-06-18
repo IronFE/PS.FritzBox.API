0f6def063d68872d324df3ad557172838693c1e4
inflames2k <inflames2k@web.de>
2018-06-18 22:43:28 +0200

    Common refactorings

--- PS.FritzBox.API/IGD/WANDevice/WANConnectionDevice/WANIPConnectionService.cs
+++ PS.FritzBox.API/IGD/WANDevice/WANConnectionDevice/WANIPConnectionService.cs
@@ -1,5 +1,6 @@
 using PS.FritzBox.API.Base;
 using PS.FritzBox.API.WANDevice.WANConnectionDevice;
+using PS.FritzBox.WANDevice.WANConnectionDevice;
 using System;
 using System.Collections.Generic;
 using System.Linq;
