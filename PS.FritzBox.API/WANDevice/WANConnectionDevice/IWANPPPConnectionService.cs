aebfcdf8fb34c3b947e652bd4d026bd79b61b2aa
inflames2k <inflames2k@web.de>
2018-06-18 23:07:13 +0200

    Common refactorings

---
+++ PS.FritzBox.API/WANDevice/WANConnectionDevice/IWANPPPConnectionService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using PS.FritzBox.API.WANDevice.WANConnectionDevice;
+
+namespace PS.FritzBox.API.WANDevice.WANConnectionDevice
+{
+    public interface IWANConnectionInterface
+    {
+        Task AddPortMappingAsync(AddPortMappingRequest request);
+        Task DeletePortMappingAsync(DeletePortMappingRequest request);
+        Task ForceTerminationAsync();
+        Task<GetConnectionTypeInfoResult> GetConnectionTypeInfoAsync();
+        Task<GetExternalIPAddressResult> GetExternalIPAddressAsync();
+        Task<GetGenericPortMappingEntryResult> GetGenericPortMappingEntryAsync(GetGenericPortMappingEntryRequest request);
+        Task<GetNATRSIPStatusResult> GetNATRSIPStatusAsync();
+        Task<GetSpecificPortMappingEntryResult> GetSpecificPortMappingEntryAsync(GetSpecificPortMappingEntryRequest request);
+        Task RequestConnectionAsync();
+        Task SetConnectionTypeAsync(SetConnectionTypeRequest request);
+    }
+}
