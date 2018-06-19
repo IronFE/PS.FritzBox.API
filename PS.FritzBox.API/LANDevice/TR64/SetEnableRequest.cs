c9fac8a5474b87c4519c11b248be4d0a7eb250ad
inflames2k <inflames2k@web.de>
2018-06-19 20:38:10 +0200

    Common refactorings.

---
+++ PS.FritzBox.API/LANDevice/TR64/SetEnableRequest.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PS.FritzBox.API.LANDevice.TR64
+{
+    /// <summary>
+    /// request object for SetEnable
+    /// </summary>
+    public class SetEnableRequest
+    {
+        /// <summary>
+        /// gets or sets the Enable
+        /// </summary>
+        public bool Enable { get; set;}
+    }
+}
